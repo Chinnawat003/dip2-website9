68c27c5228de26f9c1b179e8e1087285dcf7fefe
Chinnawat2003 <chinnawat1926@gmail.com>
2022-01-04 15:38:45 +0700

    Update 1

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+ 
+    <nav class="navbar navbar-expand-lg navbar-light bg dark text-whitl">
+    <div class="container-fluid p-4">
+        <div class="mx-auto">
+            Copyright @copy 2020-2021 by Chinnawat | All right reserved
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include("includes/header.php"); ?>
+<?php include("includes/navbar.php"); ?>
+<?php include("includes/headertop.php"); ?>
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                 <h1>Chinnawat Rattanamun</h1>
+                 <a class="btn btn-primary" href="login.php" role="button">Login</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php include("includes/footer.php"); ?>
