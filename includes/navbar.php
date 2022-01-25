63a16df15d3a455ffdfc50efa4ea151695bdf344
Chinnawat2003 <chinnawat1926@gmail.com>
2022-01-25 17:39:26 +0700

    Updata 1

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,17 +1,19 @@
-<nav class="navbar navbar-expand-lg navbar-dark sticky-top shadow" style="background-color: #0000A0;">
+<nav class="navbar navbar-expand-lg navbar-dark shadow sticky-top" style="background-color: #3a0286;">
   <div class="container">
-    <a class="navbar-brand" href="#">Chinnawat</a>
+    <a class="navbar-brand" href="index.php">Chinnawat</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php">หน้าแรก</a>
         </li>
+
         <li class="nav-item">
-          <a class="nav-link" href="#">เกี่ยงกับเรา</a>
+          <a class="nav-link" href="#">เกี่ยวกับเรา</a>
         </li>
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             เมนู
@@ -25,9 +27,11 @@
             <li><a class="dropdown-item" href="#">ออกจากระบบ</a></li>
           </ul>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="login.php">Login</a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="register.php">Register</a>
         </li>
--- index.php
+++ index.php
@@ -4,59 +4,9 @@
 
 <?php include("includes/slider.php"); ?>
 
+
 <?php include("includes/section.php"); ?>
 
-    <div class="container shadow-lg mt-3 p-2 bg-body rounded">
-          <div class="conteiner">
-            <div class="row ">
-                <div class="col-md-12 ">
-                <h2 class="text-center "><span class="text-primary">บริการชุมชน</span> ของเรา</h2>
-            <div class="underline mx-auto"></div>
-                <p class="text-center">อาชีวศึกษา</p>
-            </div>
-        </div>
-    </div>
-<!-- Card1 -->
-<div class="row">
-        <div class="col-md-4">
-          <div class="card" style="width: 25rem;">
-         <img src="image/slider/CAT.png" class="card-img-top" alt="...">
-         <div class="card-body">
-         <h2 class="text-center">ส่งเสริมสนับสนุนและพัฒนา</h2>
-         <div class="underline "></div>
-         <p class="card-text">ศูนย์บ่มเพาะวิสาหกิจเพื่อการศึกษาหมายถึงหน่วยงานที่สำนักคณะกรรมการการอาชีวศึกษา 
-             จัดตั้งขึ้นในทุกๆสถานศึกษาในสังกัดโดยมีวัตถุประสงค์เพื่อส่งเสริมสนับสนุนและพัฒนาขีด ความสามารถของนักเรียนนักศึกษาประชาชนทั่วไปที่สนใจให้สามารถนำความรู้ ทักษะวิชาชีพ 
-            เพื่อสร้างเสริมประสบการณ์เชิงพาณิชย์ให้มีความพร้อมที่จะเริ่มประกอบธุระกิจของตนเอง หรือพัฒนาต่อยอดธุระกิจได้อย่างมั่นคงและยั่งยืน</p>
-            <a href="#" class="btn btn-primary">Read Me</a>
-        </div>
-     </div>
-</div>
-<!-- Card2 -->
-    <div class="col-md-4">
-        <div class="card" style="width: 25rem;">
-         <img src="image/slider/Dogmeme.png" class="card-img-top" alt="...">
-         <div class="card-body">
-         <h2 class="text-center">การให้คำปรึกษาด้านธุรกิจ</h2>
-         <div class="underline "></div>
-         <p class="card-text"><li>บริการให้คำปรึกษา และการดูแลอย่างใกล้ชิดจากเจ้าหน้าที่บ่มเพาะวิสาหกิจ ในเรื่องการจัดตั้ง แผนธุรกิจ และเริ่มต้นดำเนินธุรกิจ</li>
-<li>บริการเป็นตัวกลางในการประสานงานแนะนำทีมที่ปรึกษาเฉพาะทาง</li>
-<li>บริการฝึกอบรม ให้ความรู้พื้นฐานและกิจกรรมเสริมสร้างความเข็มแข็งในการดำเนินธุรกิจประกอบด้วย การตลาด การเงินการบัญชี การผลิต การบริหาร 
-จัดการวิทยาศาสตร์และเทคโนโลยี เพื่อที่จะได้ทำแผนธุรกิจได้โดยถูกต้อง</li></p>
-            <a href="#" class="btn btn-primary">Read Me</a>
-        </div>
-     </div>
- </div>
-<!-- card3 -->
-        <div class="col-md-4">
-        <div class="card" style="width: 25rem;">
-         <img src="image/slider/Dog.png" class="card-img-top" alt="...">
-         <div class="card-body">
-         <h2 class="text-center">การบ่มเพาะฯ</h2>
-         <div class="underline "></div>
-         <p class="card-text"><li>การบ่มเพาะแบบ in-wall incubation คือ ผู้ถูกบ่มเพาะจะต้องเข้ามาอยู่ในศูนย์บ่มเพาะวิสาหกิจ ที่ศูนย์บ่มเพาะฯจัดเตรียมไว้ให้เต็มเวลาของช่วงระยะการบ่มเพาะ ตลอดจนมีที่ปรึกษาเป็นพี่เลี้ยงดูแลให้ตลอดช่วงเวลาของการบ่มเพาะ</li>
-         <li>การบ่มเพาะแบบ out-wall incubation คือ ผู้รับการบ่มเพาะมีสถานที่ตั้งธุรกิจ มีสำนักงาน อุปกรณ์เครื่องมือ เครื่องจักรของตนเอง ต้องการเพียงการบริการปรึกษาแนะนำ จากเจ้าหน้าที่บ่มเพาะวิสาหกิจ และที่ปรึกษาเฉพาะทางซึ่งให้คำปรึกษาในเชิงลึก</li></p>
-            <a href="#" class="btn btn-primary">Read Me</a>
-        </div>
-    </div>
-  </div>
+<?php include("includes/section2.php");?>
+
 <?php include("includes/footer.php"); ?>
